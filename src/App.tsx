871c5626d07e9143dca6402b1f4c9c2fdac9b4ae
HB <super_hbr@hotmail.com>
2026-01-01 20:20:22 -0500

    temp dumping constant ids for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import * as CONST from "../constants/Constants.ts";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Dragonsweeper Clone</h1>
+      <p>Energy Scroll ID: {CONST.ID_ENERGY_SCROLL}</p>
+      <p>Vision ID: {CONST.ID_VISION}</p>
+      <p>Vision Scroll ID: {CONST.ID_VISION_SCROLL}</p>
+      <p>Mine Scroll ID: {CONST.ID_MINE_SCROLL}</p>
+      <p>Rat ID: {CONST.ID_RAT}</p>
+      <p>Bat ID: {CONST.ID_BAT}</p>
+      <p>Skeleton ID: {CONST.ID_SKELETON}</p>
+      <p>Gargoyle ID: {CONST.ID_GARGOYLE}</p>
+      <p>Green Slime ID: {CONST.ID_GREEN_SLIME}</p>
+      <p>Troll ID: {CONST.ID_TROLL}</p>
+      <p>Knight ID: {CONST.ID_KNIGHT}</p>
+      <p>Purple Slime ID: {CONST.ID_PURPLE_SLIME}</p>
+      <p>Titan ID: {CONST.ID_TITAN}</p>
+      <p>Lich ID: {CONST.ID_LICH}</p>
+      <p>Gazer ID: {CONST.ID_GAZER}</p>
+      <p>Wizard ID: {CONST.ID_WIZARD}</p>
+      <p>Mine ID: {CONST.ID_MINE}</p>
+      <p>Dragon ID: {CONST.ID_DRAGON}</p>
+      <p>Gnome ID: {CONST.ID_GNOME}</p>
+      <p>Chest ID: {CONST.ID_CHEST}</p>
+      <p>Mimic ID: {CONST.ID_MIMIC}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
